32c38d4bb2005780b88daa91a65ef4bbea7ef018
Karol Kulik <karolkulik@protonmail.com>
2020-11-10 22:37:08 +0100

    Created mock index page with simple buttons to test environment

---
+++ WebContent/index.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Food Ordering Application</title>
+</head>
+<body>
+<form action="loginServlet" method="post">
+<input type="submit" value="Log in">
+</form>
+
+<form action="register" method="post">
+<input type="submit" value="Create Account">
+</form>
+
+<form action="menu" method="post">
+<input type="submit" value="Menu">
+</form>
+
+
+</body>
+</html>
---
+++ src/com/ztus/ServletTest.java
@@ -0,0 +1,35 @@
+package com.ztus;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ServletTest")
+public class ServletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ServletTest() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		try {
+			out.println("<h3> Test </h3>");
+		}finally {
+			out.close();
+		}
+		
+	}
+
+}
